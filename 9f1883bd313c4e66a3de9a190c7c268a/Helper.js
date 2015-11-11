a19abd3b1575d1a18ae7427e0bd963560650e48a
TFimpel <fimpeltobias@gmail.com>
2015-11-10 22:31:59 -0600

    Update Helper.js

--- 9f1883bd313c4e66a3de9a190c7c268a/Helper.js
+++ 9f1883bd313c4e66a3de9a190c7c268a/Helper.js
@@ -1,11 +1,14 @@
 //------------------------------------------------------------------------------
-
+//SOME TESTNG STUUF HERE.
 function helperSayHi(){
     console.log("Helper says Hi.")
 }
 
-//------------------------------------------------------------------------------
+function writeSyncLog(){
+    syncLogFolder.writeFile("syncLog.txt","Offline Geodatabase last synced with server when this file was last modified. Very basic, I now. But hey, no annoying file lockin issues...it just works :)")
+}
 
+//------------------------------------------------------------------------------
 //take actions based on whetehr user already has local copies of tpk and gdb
 function doorkeeper(){
     updatesCheckfile.refresh()
@@ -16,7 +19,7 @@ function doorkeeper(){
         gdbinfobuttontext.text = " Download floor plan operational layers to be able to proceed. "
     }
     else if (updatesCheckfile.exists) {
-        //TODO: if layer are added to the map lastmodified date doesn't chnage. Poible solution: a removAllLayers() function equivlent to addAllLayers()
+        //TODO: if layer are added to the map lastmodified date doesn't chnage. Poible solution: a removAllLayers() function equivlent to addAllLayers()-->doesnt work. need to cange this date reporting thing (write to other json file)
         gdbinfobuttontext.text = " Download updates for floor plan operational layers. Last updates downloaded " + updatesCheckfile.lastModified.toLocaleString("MM.dd.yyyy hh:mm ap") + "."
     }
     else {gdbinfobuttontext.text = " Download updates for floor plan operational layers. Last updates downloaded " + gdbfile.lastModified.toLocaleString("MM.dd.yyyy hh:mm ap") + "."
@@ -62,11 +65,17 @@ function selectBuildingOnMap(x,y) {
         infocontainer.visible = true;
         if (selectedFeatureId != currentBuildingObjectID){
             currentBuildingObjectID = selectedFeatureId
-            var bldgName = localBuildingsLayer.featureTable.feature(selectedFeatureId).attributeValue(bldglyr_namefield)
-            var bldgNumber = localBuildingsLayer.featureTable.feature(selectedFeatureId).attributeValue(bldglyr_bldgidfield)
+            var bldgName = localBuildingsLayer.featureTable.feature(selectedFeatureId).attributeValue(bldgLyr_bldgIdField)
+            var bldgNumber = localBuildingsLayer.featureTable.feature(selectedFeatureId).attributeValue(bldgLyr_bldgIdField)
             infotext.text = bldgName + " (#" + bldgNumber + ")"
             localLinesTable.queryFeatures("OBJECTID > 0")//this will trigger the floor slider functionailty
             localRoomsTable.queryFeatures("OBJECTID > 0")//this will trigger the floor slider functionailty
             }
     }
+}
+
+//----------------------------------------------------------------------
+//for keeping track when the offline geodatabase has been synced last
+function writeSyncLog(){
+    syncLogFolder.writeFile("syncLog.txt","Offline Geodatabase last synced with server when this file was last modified. Very basic, I now. But hey, no annoying file locking issues...it just works :)")
 }
